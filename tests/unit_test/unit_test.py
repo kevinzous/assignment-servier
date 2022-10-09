3edc900530ceeed1a459d1c5e2fb28221a1dfee7
kevinzhou <kevin75ha@hotmail.fr>
2022-10-10 00:28:27 +0200

    init for get_journal_mentionning_most_drugs

--- tests/unit_test/unit_test.py
+++ tests/unit_test/unit_test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-from bin.build_graph import cli
+from bin.build_graph import cli as cli_build_graph
+from bin.get_journal_mentionning_most_drugs import cli as cli_get_journal_mentionning_most_drugs
 
 
 def test_build_graph():
-    cli()
+    cli_build_graph()
+
+def test_get_journal_mentionning_most_drugs():
+    cli_get_journal_mentionning_most_drugs()
