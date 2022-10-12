14409b037868e4903316002d7774e3f82cf4cd13
kevinzhou <kevin75ha@hotmail.fr>
2022-10-13 00:04:25 +0200

    add unit test

--- tests/unit_test/unit_test.py
+++ tests/unit_test/unit_test.py
@@ -4,10 +4,28 @@ from bin.get_journal_mentionning_most_drugs import (
     cli as cli_get_journal_mentionning_most_drugs,
 )
 
-
+import pytest
+from pandas import read_json,read_csv
 def test_build_graph():
     cli_build_graph()
 
 
 def test_get_journal_mentionning_most_drugs():
     cli_get_journal_mentionning_most_drugs()
+
+def test_data_json():
+    with pytest.raises(FileNotFoundError):
+        read_json("data/source/pubmed_cor.json")
+    with pytest.raises(json.decoder.JSONDecodeError):
+        # only show the first error
+        read_json("data/source/pubmed_cor.json")
+        read_json("data/source/pubmed_cor.yaml")
+
+# def test_data_json():
+#     with pytest.raises(FileNotFoundError):
+#         read_json(file_path="data/source/pubmed.json")
+#     with pytest.raises(json.decoder.JSONDecodeError):
+#         # only show the first error
+#         read_json(file_path="source/data/sample_invalid.json")
+#         read_json(file_path="source/data/sample_invalid.yaml")
+            
